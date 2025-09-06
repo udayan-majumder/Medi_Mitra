ae03b64529e2fdf27cd111964c671f78659625d8
udayan-majumder <udayanmajumder1983@gmail.com>
2025-09-06 08:28:11 +0530

    Added multilanguage support using DictStore

--- client/src/app/auth/login/page.jsx
+++ client/src/app/auth/login/page.jsx
@@ -1,8 +1,14 @@
+"use client"
+
 import React from 'react'
+import { UserStore } from '@/hooks/userauth.hooks'
+import { LanguageStore } from '@/store/Dictionarystore'
 
  export default function LoginPage() {
+  const {LanguageType} = UserStore()
+  const {Language} = LanguageStore()
   return (
-    <div> this is login page </div>
+    <div>this is login page testing {Language?.[LanguageType]?.username} : {LanguageType} language</div>
   )
 }
 
--- client/src/hooks/userauth.hooks.jsx
+++ client/src/hooks/userauth.hooks.jsx
@@ -1,52 +1,62 @@
 "use client";
 import {
-  useContext,
-  createContext,
-  Children,
-  useState,
-  useEffect,
+    useContext,
+    createContext,
+    Children,
+    useState,
+    useEffect,
 } from "react";
 import axios from "axios";
 
 export const UserContext = createContext(null);
 
 export const UserStore = () => {
-  const context = useContext(UserContext);
-  if (context === null) {
-    throw new Error({ User: false });
-  }
-  return context;
+    const context = useContext(UserContext);
+    if (context === null) {
+        throw new Error({ User: false });
+    }
+    return context;
 };
 
 const Wrapper = ({ children }) => {
-  const [User, setUser] = useState(null);
+    const [User, setUser] = useState(null);
+    const [LanguageType, setLanguageType] = useState(null);
 
-  const GetUserDetails = async () => {
-    try {
-      const res = await axios.get(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/user/userdetails`,
-        {
-          withCredentials: true,
+    const GetUserDetails = async () => {
+        try {
+            const res = await axios.get(
+                `${process.env.NEXT_PUBLIC_SERVER_URL}/user/userdetails`,
+                {
+                    withCredentials: true,
+                }
+            );
+            if (res?.data?.id) {
+                setUser(res?.data);
+            }
+        } catch (e) {
+            setUser(null);
         }
-      );
-      if (res?.data?.id) {
-        setUser(res?.data);
-      }
-    } catch (e) {
-        setUser(null);
-      
-    }
-  };
+    };
 
-  useEffect(() => {
-    GetUserDetails();
-  }, []);
+    useEffect(() => {
+        GetUserDetails();
+        const Ltype = localStorage.getItem("languagetype");
+        if (!Ltype) {
+            setLanguageType("english");
+        } else {
+            setLanguageType(Ltype);
+        }
+    }, []);
 
-  return (
-    <UserContext.Provider value={{ User, setUser }}>
-      {children}
-    </UserContext.Provider>
-  );
+    useEffect(() => {
+        localStorage.setItem("languagetype", LanguageType);
+    }, [LanguageType]);
+
+    return (
+        <UserContext.Provider value={{ User, setUser, LanguageType , setLanguageType }}>
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 export default Wrapper;
