3ae7a315cf36fc4c923f0db270d38d228475d83c
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2025-09-13 20:35:44 +0530

    added chat_history functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,10 +4,16 @@ import RegisterFunction from "../controllers/register.controller.js";
 import { MiddleWareFunction } from "../middleware/verifytoken.middleware.js";
 import UserDetailsFunction from "../controllers/userdetails.controller.js";
 import LogoutFunction from "../controllers/logout.controller.js";
+import {
+  AddToChatHistory,
+  GetChatFunction,
+} from "../controllers/chat.controller.js";
 const userRouter = Router();
 
 userRouter.post("/login", LoginFunction);
 userRouter.post("/register", RegisterFunction);
 userRouter.get("/userdetails", MiddleWareFunction, UserDetailsFunction);
 userRouter.get("/logout", LogoutFunction);
+userRouter.get("/get-chat", MiddleWareFunction, GetChatFunction);
+userRouter.post("/add-chat", MiddleWareFunction, AddToChatHistory);
 export default userRouter;
