630bac558a48b6ff66a414b64e401845b5581edb
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2025-09-17 22:34:14 +0530

    Add: Labels for form input in pharmacy stock

--- client/src/app/pharmacy/page.js
+++ client/src/app/pharmacy/page.js
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import MedicalLoader from "@/Components/MedicalLoader";
 
-export default function App(){
-const router = useRouter()
-const [currentPage,setPage] = useState("/pharmacy/home")
-    useEffect(()=>{
-        router.replace("/pharmacy/home")
-    },[])
+export default function App() {
+  const router = useRouter();
+  const [currentPage, setPage] = useState("/pharmacy/home");
+  useEffect(() => {
+    router.replace("/pharmacy/home");
+  }, []);
 
-return <div>Redirecting...</div>
-
+  return <MedicalLoader />;
 }
